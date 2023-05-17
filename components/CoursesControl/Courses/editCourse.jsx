cb8542cf6150cf0f5e33a71aedad832235385b77
annapetrosyan1 <aptrsn@yandex.ru>
2023-05-17 13:14:12 +0500

    add new user of connected courses

--- components/CoursesControl/Courses/editCourse.jsx
+++ components/CoursesControl/Courses/editCourse.jsx
@@ -1,343 +1,343 @@
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import Image from "next/image"
+import { useEffect, useState } from "react"
 
 import {
-  Card,
-  Center,
-  Group,
-  Input,
-  InputWrapper,
-  LoadingOverlay,
-  MultiSelect,
-  Tabs,
-  Text,
-  useMantineTheme,
-} from "@mantine/core";
-import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
-import { showNotification } from "@mantine/notifications";
-import { nanoid } from "nanoid";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import { Check, Error404, Photo, Upload, X } from "tabler-icons-react";
-import { Days } from "../Days";
-import RichTextEditor from "/components/RichText";
-import axios from "/utils/rest";
+	Card,
+	Center,
+	Group,
+	Input,
+	InputWrapper,
+	LoadingOverlay,
+	MultiSelect,
+	Tabs,
+	Text,
+	useMantineTheme,
+} from "@mantine/core"
+import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone"
+import { showNotification } from "@mantine/notifications"
+import { nanoid } from "nanoid"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
+import { Check, Error404, Photo, Upload, X } from "tabler-icons-react"
+import { Days } from "../Days"
+import RichTextEditor from "/components/RichText"
+import axios from "/utils/rest"
 
 export const EditCourse = ({
-  opened,
-  setOpened,
-  updateCoursesList,
-  editCourseId,
+	opened,
+	setOpened,
+	updateCoursesList,
+	editCourseId,
 }) => {
-  const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(false)
 
-  const [editError, setEditError] = useState("");
-  const [nameError, setNameError] = useState("");
+	const [editError, setEditError] = useState("")
+	const [nameError, setNameError] = useState("")
 
-  const [nameDefaultValue, setNameDefaultValue] = useState("");
+	const [nameDefaultValue, setNameDefaultValue] = useState("")
 
-  const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
-  const [createObjectURL, setCreateObjectURL] = useState(null);
+	const [description, setDescription] = useState("")
+	const [image, setImage] = useState("")
+	const [createObjectURL, setCreateObjectURL] = useState(null)
 
-  const [users, setUsers] = useState([]);
-  const [selectedUsers, setSelectedUsers] = useState([]);
+	const [users, setUsers] = useState([])
+	const [selectedUsers, setSelectedUsers] = useState([])
 
-  const theme = useMantineTheme();
+	const theme = useMantineTheme()
 
-  useEffect(() => {
-    if (opened) {
-      axios
-        .get("/users")
-        .then((res) => {
-          setUsers(res.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-        .finally(() => {});
-    }
-  }, [opened]);
+	useEffect(() => {
+		if (opened) {
+			axios
+				.get("/users")
+				.then((res) => {
+					setUsers(res.data)
+          //новый массив юзеров
+				})
+				.catch((error) => {
+					console.log(error)
+				})
+				.finally(() => {})
+		}
+	}, [opened])
 
-  useEffect(() => {
-    if (editCourseId !== -1) {
-      setDescription("");
-      setSelectedUsers([]);
-      setLoading(true);
-      axios
-        .get(`/courses/${editCourseId}`)
-        .then((res) => {
-          if (res.status === 200) {
-            setNameDefaultValue(res.data.name);
-            setDescription(res.data.description);
-            setImage(res.data.image);
-            axios
-              .get(`/courses/${editCourseId}/users`)
-              .then((res) => {
-                if (res.status === 200) {
-                  setSelectedUsers(res.data);
-                } else {
-                  setEditError("Ошибка редактирования курса, попробуйте позже");
-                }
-              })
-              .catch((error) => {
-                console.log(error);
-                if (error.response.status === 410) {
-                  setEditError("Курс не найден, попробуйте позже");
-                }
-              })
-              .finally(() => {
-                setLoading(false);
-              });
-          } else {
-            setEditError("Ошибка редактирования курса, попробуйте позже");
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-          if (error.response.status === "404") {
-            showNotification({
-              title: "Курс не найден",
-              autoClose: 3500,
-              color: "red",
-              icon: <Error404 size={18} />,
-            });
-          } else {
-            showNotification({
-              title: "Ошибка получения курса",
-              autoClose: 3500,
-              color: "red",
-              icon: <Error404 size={18} />,
-            });
-          }
-          setOpened(false);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }
-  }, [editCourseId, setOpened, setSelectedUsers]);
+	useEffect(() => {
+		if (editCourseId !== -1) {
+			setDescription("")
+			setSelectedUsers([])
+			setLoading(true)
+			axios
+				.get(`/courses/${editCourseId}`)
+				.then((res) => {
+					if (res.status === 200) {
+						setNameDefaultValue(res.data.name)
+						setDescription(res.data.description)
+						setImage(res.data.image)
+						axios
+							.get(`/courses/${editCourseId}/users`)
+							.then((res) => {
+								if (res.status === 200) {
+									setSelectedUsers(res.data)
+								} else {
+									setEditError("Ошибка редактирования курса, попробуйте позже")
+								}
+							})
+							.catch((error) => {
+								console.log(error)
+								if (error.response.status === 410) {
+									setEditError("Курс не найден, попробуйте позже")
+								}
+							})
+							.finally(() => {
+								setLoading(false)
+							})
+					} else {
+						setEditError("Ошибка редактирования курса, попробуйте позже")
+					}
+				})
+				.catch((error) => {
+					console.log(error)
+					if (error.response.status === "404") {
+						showNotification({
+							title: "Курс не найден",
+							autoClose: 3500,
+							color: "red",
+							icon: <Error404 size={18} />,
+						})
+					} else {
+						showNotification({
+							title: "Ошибка получения курса",
+							autoClose: 3500,
+							color: "red",
+							icon: <Error404 size={18} />,
+						})
+					}
+					setOpened(false)
+				})
+				.finally(() => {
+					setLoading(false)
+				})
+		}
+	}, [editCourseId, setOpened, setSelectedUsers])
 
-  const onSelect = async (selected) => {
-    const user = users.find(
-      (user) => user.email === selected[selected.length - 1]
-    );
-    await axios
-      .post(`/courses/${editCourseId}/users`, user)
-      .then((result) => {
-        setSelectedUsers((prev) => [...prev, result.data.body]);
-        console.log("result", result);
-      })
-      .catch(console.log);
-  };
-  const getIconColor = (status, theme) => {
-    return status.accepted
-      ? theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6]
-      : status.rejected
-      ? theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]
-      : theme.colorScheme === "dark"
-      ? theme.colors.dark[0]
-      : theme.colors.gray[7];
-  };
+	const onSelect = async (selected) => {
+		const user = users.find(
+			(user) => user.email === selected[selected.length - 1]
+		)
+		await axios
+			.post(`/courses/${editCourseId}/users`, user)
+			.then((result) => {
+				setSelectedUsers([...result.data])
+			})
+			.catch(console.log)
+	}
+	const getIconColor = (status, theme) => {
+		return status.accepted
+			? theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6]
+			: status.rejected
+			? theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]
+			: theme.colorScheme === "dark"
+			? theme.colors.dark[0]
+			: theme.colors.gray[7]
+	}
 
-  const ImageUploadIcon = ({ status, ...props }) => {
-    if (status.accepted) {
-      return <Upload {...props} />;
-    }
+	const ImageUploadIcon = ({ status, ...props }) => {
+		if (status.accepted) {
+			return <Upload {...props} />
+		}
 
-    if (status.rejected) {
-      return <X {...props} />;
-    }
+		if (status.rejected) {
+			return <X {...props} />
+		}
 
-    return <Photo {...props} />;
-  };
+		return <Photo {...props} />
+	}
 
-  const dropzoneChildren = (status, theme) => (
-    <Group
-      position="center"
-      spacing="xl"
-      style={{ minHeight: 220, pointerEvents: "none" }}
-    >
-      {image ? (
-        <Image
-          src={createObjectURL ? createObjectURL : "/" + image}
-          width={125}
-          height={125}
-          alt="Изображение курса"
-        />
-      ) : (
-        <ImageUploadIcon
-          status={status}
-          style={{ color: getIconColor(status, theme) }}
-          size={80}
-        />
-      )}
-    </Group>
-  );
+	const dropzoneChildren = (status, theme) => (
+		<Group
+			position='center'
+			spacing='xl'
+			style={{ minHeight: 220, pointerEvents: "none" }}
+		>
+			{image ? (
+				<Image
+					src={createObjectURL ? createObjectURL : "/" + image}
+					width={125}
+					height={125}
+					alt='Изображение курса'
+				/>
+			) : (
+				<ImageUploadIcon
+					status={status}
+					style={{ color: getIconColor(status, theme) }}
+					size={80}
+				/>
+			)}
+		</Group>
+	)
 
-  const saveCourse = (e) => {
-    e.preventDefault();
-    setNameError("");
-    setEditError("");
-    if (e.target.name.value === "") {
-      setNameError("Введите название курса");
-      return;
-    }
+	const saveCourse = (e) => {
+		e.preventDefault()
+		setNameError("")
+		setEditError("")
+		if (e.target.name.value === "") {
+			setNameError("Введите название курса")
+			return
+		}
 
-    if (image === "") {
-      setEditError("Выберите изображение");
-      return;
-    }
+		if (image === "") {
+			setEditError("Выберите изображение")
+			return
+		}
 
-    setLoading(true);
+		setLoading(true)
 
-    const body = new FormData();
-    body.append("name", e.target.name.value);
-    body.append("description", description);
-    if (image && image.path) {
-      body.append("image", image, `course_${nanoid()}`);
-    }
-    axios
-      .put(`/courses/${editCourseId}`, body)
-      .then((res) => {
-        if (res.status === 200) {
-          updateCoursesList(res.data.course);
-          showNotification({
-            title: "Курс изменен",
-            autoClose: 3500,
-            color: "green",
-            icon: <Check size={18} />,
-          });
-        } else {
-          setEditError("Ошибка изменения курса, попробуйте позже");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        if (error.status === 409) {
-          setEditError("Такое название курса уже занято");
-        } else {
-          setEditError("Ошибка изменения курса, попробуйте позже");
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+		const body = new FormData()
+		body.append("name", e.target.name.value)
+		body.append("description", description)
+		if (image && image.path) {
+			body.append("image", image, `course_${nanoid()}`)
+		}
+		axios
+			.put(`/courses/${editCourseId}`, body)
+			.then((res) => {
+				if (res.status === 200) {
+					updateCoursesList(res.data.course)
+					showNotification({
+						title: "Курс изменен",
+						autoClose: 3500,
+						color: "green",
+						icon: <Check size={18} />,
+					})
+				} else {
+					setEditError("Ошибка изменения курса, попробуйте позже")
+				}
+			})
+			.catch((error) => {
+				console.log(error)
+				if (error.status === 409) {
+					setEditError("Такое название курса уже занято")
+				} else {
+					setEditError("Ошибка изменения курса, попробуйте позже")
+				}
+			})
+			.finally(() => {
+				setLoading(false)
+			})
+	}
 
-  return (
-    <div>
-      {description.length > 0 && (
-        <form onSubmit={saveCourse}>
-          <div style={{ textAlign: "end" }}>
-            <button
-              style={{
-                fontSize: "12px",
-                color: "#036459",
-                fontWeight: "600",
-                marginRight: "9px",
-                backgroundColor: "white",
-                border: "2px solid #33CFBD",
-                borderRadius: "20px",
-                padding: "10px 14px",
-              }}
-              type="submit"
-            >
-              Сохранить
-            </button>
-            <button
-              style={{
-                fontSize: "12px",
-                color: "#036459",
-                fontWeight: "600",
-                backgroundColor: "white",
-                border: "2px solid #FD938E",
-                borderRadius: "20px",
-                padding: "10px 14px",
-              }}
-              onClick={() => setOpened(false)}
-            >
-              Отменить
-            </button>
-          </div>
-          <Row>
-            <Col md={4}>
-              <Card shadow="sm" padding="lg" radius="md" withBorder>
-                <InputWrapper required label="Название курса" error={nameError}>
-                  <Input
-                    type="text"
-                    name="name"
-                    value={nameDefaultValue}
-                    onChange={(e) => setNameDefaultValue(e.currentTarget.value)}
-                  />
-                </InputWrapper>
-                <Dropzone
-                  onDrop={(files) => {
-                    setImage(files[0]);
-                    setCreateObjectURL(URL.createObjectURL(files[0]));
-                  }}
-                  onReject={() => {
-                    showNotification({
-                      title: "Файл отклонен",
-                      autoClose: 3500,
-                      color: "red",
-                      icon: <X size={18} />,
-                    });
-                  }}
-                  maxSize={3 * 1024 ** 2}
-                  accept={IMAGE_MIME_TYPE}
-                  padding="xs"
-                >
-                  {(status) => dropzoneChildren(status, theme)}
-                </Dropzone>
-              </Card>
-            </Col>
-            <Col md={8}>
-              <Tabs unstyled color="#036459">
-                <Tabs.Tab label="Участники">
-                  <MultiSelect
-                    style={{
-                      border: "2px solid #33CFBD",
-                      padding: "25px",
-                      borderRadius: "8px",
-                      boxShadow: "0px 2px 20px #BBBBBB",
-                    }}
-                    value={selectedUsers}
-                    onChange={onSelect}
-                    data={users.map((el) => el.email)}
-                    label="Добавьте пользователя"
-                    placeholder="Пользователей не выбрано"
-                    searchable
-                    nothingFound="Пользователей не найдено"
-                  />
-                </Tabs.Tab>
-                <Tabs.Tab label="О курсе">
-                  <RichTextEditor
-                    name="description"
-                    value={description}
-                    onChange={(value) => {
-                      setDescription(value);
-                    }}
-                    controls={[
-                      ["bold", "italic", "underline", "link"],
-                      ["unorderedList", "orderedList"],
-                      ["h1", "h2", "h3"],
-                      ["sup", "sub"],
-                      ["alignLeft", "alignCenter", "alignRight"],
-                    ]}
-                    style={{ height: "400px", overflow: "auto" }}
-                  />
-                </Tabs.Tab>
-              </Tabs>
-            </Col>
-          </Row>
-          <LoadingOverlay visible={loading} />
-          <Center>
-            <Text color="red">{editError}</Text>
-          </Center>
-        </form>
-      )}
-      <Days opened={editCourseId} courseId={editCourseId} />
-    </div>
-  );
-};
+	return (
+		<div>
+			{description.length > 0 && (
+				<form onSubmit={saveCourse}>
+					<div style={{ textAlign: "end" }}>
+						<button
+							style={{
+								fontSize: "12px",
+								color: "#036459",
+								fontWeight: "600",
+								marginRight: "9px",
+								backgroundColor: "white",
+								border: "2px solid #33CFBD",
+								borderRadius: "20px",
+								padding: "10px 14px",
+							}}
+							type='submit'
+						>
+							Сохранить
+						</button>
+						<button
+							style={{
+								fontSize: "12px",
+								color: "#036459",
+								fontWeight: "600",
+								backgroundColor: "white",
+								border: "2px solid #FD938E",
+								borderRadius: "20px",
+								padding: "10px 14px",
+							}}
+							onClick={() => setOpened(false)}
+						>
+							Отменить
+						</button>
+					</div>
+					<Row>
+						<Col md={4}>
+							<Card shadow='sm' padding='lg' radius='md' withBorder>
+								<InputWrapper required label='Название курса' error={nameError}>
+									<Input
+										type='text'
+										name='name'
+										value={nameDefaultValue}
+										onChange={(e) => setNameDefaultValue(e.currentTarget.value)}
+									/>
+								</InputWrapper>
+								<Dropzone
+									onDrop={(files) => {
+										setImage(files[0])
+										setCreateObjectURL(URL.createObjectURL(files[0]))
+									}}
+									onReject={() => {
+										showNotification({
+											title: "Файл отклонен",
+											autoClose: 3500,
+											color: "red",
+											icon: <X size={18} />,
+										})
+									}}
+									maxSize={3 * 1024 ** 2}
+									accept={IMAGE_MIME_TYPE}
+									padding='xs'
+								>
+									{(status) => dropzoneChildren(status, theme)}
+								</Dropzone>
+							</Card>
+						</Col>
+						<Col md={8}>
+							<Tabs unstyled color='#036459'>
+								<Tabs.Tab label='Участники'>
+									<MultiSelect
+										style={{
+											border: "2px solid #33CFBD",
+											padding: "25px",
+											borderRadius: "8px",
+											boxShadow: "0px 2px 20px #BBBBBB",
+										}}
+										value={selectedUsers}
+										onChange={onSelect}
+										data={users.map((el) => el.email)}
+										label='Добавьте пользователя'
+										placeholder='Пользователей не выбрано'
+										searchable
+										nothingFound='Пользователей не найдено'
+									/>
+								</Tabs.Tab>
+								<Tabs.Tab label='О курсе'>
+									<RichTextEditor
+										name='description'
+										value={description}
+										onChange={(value) => {
+											setDescription(value)
+										}}
+										controls={[
+											["bold", "italic", "underline", "link"],
+											["unorderedList", "orderedList"],
+											["h1", "h2", "h3"],
+											["sup", "sub"],
+											["alignLeft", "alignCenter", "alignRight"],
+										]}
+										style={{ height: "400px", overflow: "auto" }}
+									/>
+								</Tabs.Tab>
+							</Tabs>
+						</Col>
+					</Row>
+					<LoadingOverlay visible={loading} />
+					<Center>
+						<Text color='red'>{editError}</Text>
+					</Center>
+				</form>
+			)}
+			<Days opened={editCourseId} courseId={editCourseId} />
+		</div>
+	)
+}
--- pages/api/courses/[id]/users.js
+++ pages/api/courses/[id]/users.js
@@ -1,48 +1,74 @@
-import { withIronSessionApiRoute } from "iron-session/next";
-import { sessionOptions } from "/lib/session";
+import { withIronSessionApiRoute } from "iron-session/next"
+import { sessionOptions } from "/lib/session"
+import database from "/utils/database"
 
-import database from "/utils/database";
+async function getConnectedCoureses(course_id) {
+	//получение user id в функции, чтобы просто вызвать, а не переписать одно и тоже
+	const connected_courses = await database
+		.select("user_id")
+		.from("connected_courses")
+		.where({ course_id })
+
+	return connected_courses
+}
 
 const courseUsersHandler = async (req, res) => {
-  const { method } = req;
-  const body = req.body;
-  const { id } = req.query;
-  const exists_course = await database
-    .select("*")
-    .from("courses")
-    .where({ id: id })
-    .limit(1);
-  if (exists_course.length <= 0) {
-    res.status(410).json({ errorMessage: "Course not exists" });
-    return;
-  }
-  switch (method) {
-    case "GET":
-      const connected_courses = await database
-        .select("user_id")
-        .from("connected_courses")
-        .where({ course_id: exists_course[0].id });
-      const connected_users = await database
-        .select("email")
-        .from("users")
-        .whereIn(
-          "id",
-          connected_courses.map((el) => el.user_id)
-        );
-      res.status(200).json(connected_users.map((el) => el.email));
-      break;
-	  //в этом case должен обрабатываться запрос, сначала запись в бд
-    case "POST":
-      await database("connected_courses").insert({
-        user_id: body.id,
-        email: body.email,
-      });
-      res.status(200).json(connected_users.map((el) => el.email));
+	const { method } = req
+	const body = req.body
+	const { id } = req.query
+	const exists_course = await database
+		.select("*")
+		.from("courses")
+		.where({ id: id })
+		.limit(1)
+	if (exists_course.length <= 0) {
+		res.status(410).json({ errorMessage: "Course not exists" })
+		return
+	}
+	switch (method) {
+		case "GET":
+			const connected_courses = await getConnectedCoureses(exists_course[0].id)
+			const connected_users = await database
+				.select("email")
+				.from("users")
+				.whereIn(
+					"id",
+					connected_courses.map((el) => el.user_id)
+				)
+			res.status(200).json(connected_users.map((el) => el.email))
+			break
+			//в этом case должен обрабатываться запрос, сначала запись в бд
+		case "POST":
+			// тут insert ом добавляем в бд, потом по логике которая тут была {получаем id курса 
+			// и по нему всех user ов подписанных на него. он дает новый массив и это отправляем в front-ui}
+			database("connected_courses")
+				.insert({
+					user_id: body.id,
+					course_id: exists_course[0].id,
+				})
+				.then((result) => console.log("result", JSPN.stringify(result)))
+			const refetched_connected_courses = await getConnectedCoureses(
+				exists_course[0].id
+			)
 
-    default:
-      res.setheader("allow", ["get"]);
-      res.status(405).end(`method ${method} not allowed`);
-  }
-};
+			const get_after_creating_new_user_for_connected_courses = await database
+				.select("email")
+				.from("users")
+				.whereIn(
+					"id",
+					refetched_connected_courses.map((el) => el.user_id)
+				)
+			res
+				.status(200)
+				.json(
+					get_after_creating_new_user_for_connected_courses.map(
+						(el) => el.email
+					)
+				)
+		default:
+			res.setheader("allow", ["get"])
+			res.status(405).end(`method ${method} not allowed`)
+	}
+}
 
-export default withIronSessionApiRoute(courseUsersHandler, sessionOptions);
+export default withIronSessionApiRoute(courseUsersHandler, sessionOptions)
